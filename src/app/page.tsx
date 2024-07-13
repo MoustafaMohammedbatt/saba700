57f2b707f2f94e0a16728d83420a3c80a8337d46
Moustafa Batt <114432989+MoustafaMohammedbatt@users.noreply.github.com>
2024-07-13 20:46:11 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
-import Breadcrumb from "@/components/Common/Breadcrumb";
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Breadcrumb from '@/components/Common/Breadcrumb';
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Home() {
+  const [playerName, setPlayerName] = useState('');
+
+  useEffect(() => {
+    const fetchPlayerName = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/GuessPlayer`);
+        setPlayerName(response.data.playerName);
+      } catch (error) {
+        console.error('Error fetching player data:', error);
+      }
+    };
+
+    fetchPlayerName();
+  }, []);
+
   return (
     <>
-    <Breadcrumb
-        pageName=" XO"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+      <Breadcrumb
+        pageName="XO"
+        description={`Player Name: ${playerName}`}
       />
     </>
   );
